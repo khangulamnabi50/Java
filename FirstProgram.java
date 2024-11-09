e3ff7aa8f588da5b8b773978fc31fc8c82f23d0a
Gulam Nabi Khan <gulamnabikhan50@gmail.com>
2024-11-10 01:05:09 +0530

    first commit

---
+++ FirstProgram.java
@@ -0,0 +1,28 @@
+
+class FirstProgram{
+    public static void main(String [] args) {
+        System.out.println("Name :- JTC");
+        System.out.println("email :- jtc@jtcindia.org"); 
+        System.out.println("job :- software");
+        System.out.println("Favourite subject :- Computer science");
+    }
+} 
+
+
+class A{
+    public static void main(String [] args) {
+        System.out.println("Name :- JTC");
+        
+    }
+} 
+class C{
+    public static void main(String [] args) {
+        System.out.println("Name :- KTC");
+        
+    }
+} 
+
+class B{
+    
+} 
+
